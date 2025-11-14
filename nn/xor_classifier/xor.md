4f6457b4727ad1a22a8c4277434f10ab54de99f5
Jimmy Shah <jimmyshah83@gmail.com>
2025-11-14 13:16:15 -0500

    Add XOR NN - WIP

--- nn/xor_classifier/xor.md
+++ nn/xor_classifier/xor.md
@@ -2,3 +2,14 @@
 
 ![XOR Model](../../assets/xor.png)
 
+### Optimizer
+
+- Get a batch from the data loader
+- Obtain the predictions from the model for the batch
+- Calculate the loss based on the difference between predictions and labels
+- Backpropagation: calculate the gradients for every parameter with respect to the loss
+- Update the parameters of the model in the direction of the gradients
+
+### Loss Function
+
+- Binary Cross Entropy
--- nn/xor_classifier/xor.py
+++ nn/xor_classifier/xor.py
@@ -1,5 +1,33 @@
 """Neural network model for XOR operation."""
 from torch import nn
+import torch
+from torch.utils import data
+
+
+class XORDataset(data.Dataset):    
+    """Dataset for XOR operation."""
+    def __init__(self, size, std=0.1):
+        """Initialize the dataset."""
+        self.size = size
+        self.std = std
+        self.generate_continuous_xor()
+        
+    def generate_continuous_xor(self):
+        """Generate continuous XOR data."""
+        x_data = torch.randint(0, 2, (self.size, 2), dtype=torch.float32)
+        label = (x_data.sum(dim=1) == 1).to(torch.long)
+        # Add noise to the data
+        x_data += torch.randn(x_data.shape) * self.std
+        self.data = x_data
+        self.label = label
+        
+    def __len__(self):
+        """Return the length of the dataset."""
+        return self.size
+    
+    def __getitem__(self, index):
+        """Return the data and label at the given index."""
+        return self.data[index], self.label[index]
 
 
 class XORModel(nn.Module):
@@ -23,7 +51,18 @@ def main():
     """Main function to run the XOR model."""
     model = XORModel(num_inputs=2, num_hidden=4, num_outputs=1)
     print(model)
-
+    for name, param in model.named_parameters():
+        print(f"Parameter: {name}:, Shape: {param.shape}")
+        
+    dataset = XORDataset(size=200)
+    print(f"Data: {dataset[0][0]}, Label: {dataset[0][1]}")
+    
+    dataloader = data.DataLoader(dataset, batch_size=10, shuffle=True)
+    data_inputs, data_labels = next(iter(dataloader))
+    print(f"Data inputs: {data_inputs}, Data labels: {data_labels}")
+    
+    loss_function = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
 if __name__ == "__main__":
     main()
